3205889684b542138840acffe893a5649218467c
star <KurosawaReo.Job@gmail.com>
2025-04-08 14:28:00 +0900

    盤面をどうするか考えてる途中

--- GameJam2025_spr_class/Assets/Scripts/BoardManager.cs
+++ GameJam2025_spr_class/Assets/Scripts/BoardManager.cs
@@ -1,16 +1,98 @@
+using Gloval;
 using UnityEngine;
 
+/// <summary>
+/// 盤面データ.
+/// </summary>
+public class BoardData
+{
+    //private変数.
+    private BoardArea  m_area;    //誰のエリアか.
+    private GameObject m_areaObj; //陣地として設置するobjのデータ.
+
+    //set, get.
+    public BoardArea area {
+        get => m_area; 
+        set => m_area = value; 
+    }
+    public GameObject areaObj
+    {
+        get => m_areaObj;
+        set => m_areaObj = value;
+    }
+}
+
+/// <summary>
+/// 盤面管理プログラム.
+/// </summary>
 public class BoardManager : MonoBehaviour
 {
+    [Header("- prefab -")]
+    [SerializeField] GameObject prfbBoardBack; //盤面の背景prefab.
+    [SerializeField] GameObject prfbAreaPly;   //プレイヤー陣地prefab.
+    [SerializeField] GameObject prfbAreaEnm;   //敵陣地prefab.
+    [Space]
+    [SerializeField] GameObject prfbInObj;     //prefabを入れるところ.
 
+    //盤面データ.
+    BoardData[,] board = new BoardData[Gl_Const.BOARD_WID, Gl_Const.BOARD_HEI];
 
     void Start()
     {
-        
+        InitBoard();
+        BoardGenerate();
     }
 
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// 盤面データの初期化.
+    /// </summary>
+    private void InitBoard()
+    {
+        //1マスずつ.
+        for (int y = 0; y < Gl_Const.BOARD_HEI; y++) {
+            for (int x = 0; x < Gl_Const.BOARD_WID; x++) {
+
+                board[x, y].area = BoardArea.NONE; //無に設定.
+            }
+        }
+    }
+
+    /// <summary>
+    /// 盤面を生成する.
+    /// </summary>
+    private void BoardGenerate()
+    {
+        //1マスずつ.
+        for (int y = 0; y < Gl_Const.BOARD_HEI; y++) {
+            for (int x = 0; x < Gl_Const.BOARD_WID; x++) {
+
+                var obj = Instantiate(prfbBoardBack, prfbInObj.transform);
+
+                GameObject obj = null; //prefab情報を入れる用.
+
+                //データ別で生成.
+                switch (board[x, y].area) {
+                    case BoardArea.NONE:
+                        obj = Instantiate(prfbBoardBack, prfbInObj.transform);
+                        break;
+                    case BoardArea.PLAYER:
+                        obj = Instantiate(prfbAreaPly, prfbInObj.transform);
+                        break;
+                    case BoardArea.ENEMY:
+                        obj = Instantiate(prfbAreaEnm, prfbInObj.transform);
+                        break;
+
+                    default: Debug.LogError("[Error] 不正な値です。"); break;
+                }
+
+                //盤面上に設置.
+                Gl_Func.PlaceOnBoard(obj, x, y);
+            }
+        }
     }
 }
--- GameJam2025_spr_class/Assets/Scripts/Gloval.cs
+++ GameJam2025_spr_class/Assets/Scripts/Gloval.cs
@@ -5,10 +5,11 @@ namespace Gloval
     /// <summary>
     /// 盤面のマス情報.
     /// </summary>
-    public enum Board
+    public enum BoardArea
     {
-
-
+        NONE,
+        PLAYER,
+        ENEMY,
     }
 
     /// <summary>
@@ -16,8 +17,11 @@ namespace Gloval
     /// </summary>
     public static class Gl_Const
     {
-        public const int BOARD_HEI = 3;
-        public const int BOARD_WID = 5;
+        public const int   BOARD_HEI = 3;
+        public const int   BOARD_WID = 5;
+
+        //public const float BOARD_LEFTUP_X = 3;  //盤面の左上座標x.
+        //public const float BOARD_LEFTUP_Y = -2; //盤面の左上座標y.
 
         /*
         //例:
@@ -35,7 +39,22 @@ namespace Gloval
     /// </summary>
     public static class Gl_Func
     {
-        
+        /// <summary>
+        /// 盤面データを元に設置.
+        /// </summary>
+        /// <param name="_obj">設置するオブジェクト</param>
+        /// <param name="_x">盤面の座標x</param>
+        /// <param name="_y">盤面の座標y</param>
+        public static void PlaceOnBoard(GameObject _obj, int _x, int _y)
+        {
+            //サイズ取得.
+            var size = _obj.GetComponent<SpriteRenderer>().bounds.size.x;
+            //座標を計算(中央基準)
+            float x = (_x - Gl_Const.BOARD_WID/2) * size;
+            float y = (_y - Gl_Const.BOARD_HEI/2) * size;
+            //配置.
+            _obj.transform.position = new Vector2(x, y);
+        }
 
         /// <summary>
         /// ゲームプレイ終了.
